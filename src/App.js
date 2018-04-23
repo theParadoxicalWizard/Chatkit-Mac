b7c042c609a83ac46298da2acd1ee4600de58ced
Christian Nwamba <nwambachristian@gmail.com>
2018-04-23 07:29:15 +0100

    Starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to Chatkit Desktop Starter</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
