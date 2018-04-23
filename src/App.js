8da14c572de48dbd11bd1355f4632fbd9edd3854
Christian Nwamba <nwambachristian@gmail.com>
2018-04-23 07:46:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './Layout';
+import Chatbox from './Chatbox';
+import Sidebar from './Sidebar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Chatkit Desktop Starter</h1>
+        <Layout Sidebar={Sidebar} Chatbox={Chatbox} />
       </div>
     );
   }
